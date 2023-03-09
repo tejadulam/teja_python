1af49b85d4b6a5d8643ca1bc87402c882a5948ee
tejadulam <tejunld2001@gmial.com>
2023-03-09 16:12:16 +0530

    pograms

---
+++ compound_interest.py
@@ -0,0 +1,6 @@
+principle = int(input("enter principle: "))
+rate = int(input("enter rate: "))
+time = int(input("enter rate: "))
+n = int(input("enter n: "))
+compound_interst = principle*(1+(rate/100))**(n*time)
+print(compound_interst)
