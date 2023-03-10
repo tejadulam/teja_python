26e8ecaad094f67dba60c840ebd7d38ad4e6763d
tejadulam <tejunld2001@gmial.com>
2023-03-10 12:20:00 +0530

    test pograms

---
+++ listtest.py/alternatelist1.py
@@ -0,0 +1,5 @@
+r = []
+for i in range(5):
+    n = int(input("enter n:"))
+    r.append(n)
+    print(r[0::2])
---
+++ listtest.py/largest.py
@@ -0,0 +1,16 @@
+# l = []
+# for i in range(5):
+#     num = int(input("enter num: "))
+#     if (l(i)>num):
+#         num = l(i)
+#         l.append(num)
+#     print(l)
+l =[]   
+m = l[0]
+for x in l:
+    n = int(input("enter n: "))
+    if x>m:
+        m = x
+    print(m)
+    
+    
