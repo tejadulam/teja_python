4ad7a2b5b46be1eeca04b4f4608bcf5c156e6ba1
tejadulam <tejunld2001@gmial.com>
2023-03-20 12:16:05 +0530

    deleted

--- listtest.py/list_test2.py/max_min10.py
+++
@@ -1,3 +0,0 @@
-set = {5, 10, 3, 15, 2, 20}
-print(max(set))
-print(min(set))
--- listtest.py/product_oddindex.py
+++
@@ -1,6 +0,0 @@
-l = []
-for i in range(6):
-    n = int(input("enter n: "))
-    l.append(n)
-    print(l)
-    print(l[1::3])
